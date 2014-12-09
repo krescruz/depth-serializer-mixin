5decd7e68c6454e455bc1debe232ea37f7260c58
ccruz <ccruz@nomiservicios>
2014-12-09 13:06:17 -0600

    Call method 'get_serializer_class' of the Class parent

--- mixins.py
+++ mixins.py
@@ -1,8 +1,8 @@
 class DepthSerializerMixin(object):
 	"""Custom method 'get_serializer_class', set attribute 'depth' based on query parameter in the url"""
 	
-	def get_serializer_class(self):
-		serializer_class = self.serializer_class
+	def get_serializer_class(self, *args, **kwargs):
+		serializer_class = super(DepthSerializerMixin, self).get_serializer_class(*args, **kwargs)
 		query_params = self.request.QUERY_PARAMS
 		depth = query_params.get('__depth', None)
 		serializer_class.Meta.depth = int(depth) if(depth != None and depth.isdigit()) else 0
